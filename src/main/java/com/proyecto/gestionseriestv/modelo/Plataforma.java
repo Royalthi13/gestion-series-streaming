3612484a9cd33ae074b52dec7557f85d5682103a
Royalthi13 <royalthi13@gmail.com>
2025-05-20 18:41:38 +0200

    Se crea la clase Plataforma en el modelo

--- src/main/java/com/proyecto/gestionseriestv/modelo/Plataforma.java
+++ src/main/java/com/proyecto/gestionseriestv/modelo/Plataforma.java
@@ -1,5 +1,61 @@
 package com.proyecto.gestionseriestv.modelo;
 
 public class Plataforma {
+private int id;
+private String nombre;
+private String paisOrigen;
+
+
+public Plataforma(int id, String nombre, String paisOrigen) {
+	this.id = id;
+	this.nombre = nombre;
+	this.paisOrigen = paisOrigen;
+}
+
+
+public int getId() {
+	return id;
+}
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+public String getNombre() {
+	return nombre;
+}
+
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+
+public String getPaisOrigen() {
+	return paisOrigen;
+}
+
+
+public void setPaisOrigen(String paisOrigen) {
+	this.paisOrigen = paisOrigen;
+}
+
+
+@Override
+public String toString() {
+	return "Plataforma [id=" + id + ", nombre=" + nombre + ", paisOrigen=" + paisOrigen + "]";
+}
+
+
+
+
+
+
+
+
+
+
 
 }
