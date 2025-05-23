07708289305fc950b857aa962f8376d1d7853ad3
Royalthi13 <royalthi13@gmail.com>
2025-05-23 19:33:34 +0200

    creacion de clase para metodos crud series

---
+++ src/main/java/com/proyecto/gestionseriestv/modelo/SeriesDao.java
@@ -0,0 +1,11 @@
+package com.proyecto.gestionseriestv.modelo;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class SeriesDao {
+
+	
+	
+}
